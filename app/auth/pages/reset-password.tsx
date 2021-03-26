b92d156b70bb7fd5eabb81151fdbdcfbef05cf25
Gus Fune <gus@offscript.io>
2021-03-26 23:47:39 +0100

    Changed layout base

--- app/auth/pages/reset-password.tsx
+++ app/auth/pages/reset-password.tsx
@@ -1,5 +1,5 @@
 import { BlitzPage, useRouterQuery, Link, useMutation } from "blitz"
-import Layout from "app/core/layouts/Layout"
+import Layout from "app/core/layouts/Home"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import { ResetPassword } from "app/auth/validations"
