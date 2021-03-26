801c54f67e0de27f3b004141a3616f1484f36453
Gus Fune <gus@offscript.io>
2021-03-26 19:01:24 +0100

    First version with auth and PGSQL

--- blitz.config.js
+++ blitz.config.js
@@ -1,6 +1,12 @@
+const { sessionMiddleware, simpleRolesIsAuthorized } = require("blitz")
+
 module.exports = {
   poweredByHeader: false,
-  middleware: [],
+  middleware: [
+    sessionMiddleware({
+      isAuthorized: simpleRolesIsAuthorized,
+    }),
+  ],
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
