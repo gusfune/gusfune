f15853fe1d45f3e2204b645a061fd6c3cc2c697d
Gus Fune <gus@offscript.io>
2021-08-02 08:02:10 +0200

    Added sentry to project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs")
+const { createFalse } = require("typescript")
+
+const SentryWebpackPluginOptions = {
+  silent: createFalse,
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+const moduleExports = {
   reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com"],
   },
 }
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
