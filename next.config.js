323fb3cd5832f5d803d200697a17edf04eb50b41
Gus Fune <gus@offscript.io>
2021-09-03 15:03:52 +0200

    Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+const withPlugins = require("next-compose-plugins")
 const { withSentryConfig } = require("@sentry/nextjs")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+module.exports = withBundleAnalyzer({})
 
 const SentryWebpackPluginOptions = {
   silent: true,
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
+
+withSentryConfig
 
 const moduleExports = {
   reactStrictMode: true,
@@ -17,4 +24,7 @@ const moduleExports = {
   },
 }
 
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
+module.exports = withPlugins(
+  [[withSentryConfig, SentryWebpackPluginOptions], [withBundleAnalyzer]],
+  moduleExports
+)
