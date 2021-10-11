c5b82992b28c4334ed7409690a88bd946baba870
Gus Fune <gus@offscript.io>
2021-10-11 11:39:56 +0200

    Added basic GQL server

---
+++ pages/api/index.ts
@@ -0,0 +1,85 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { PrismaClient } from "@prisma/client"
+import { gql, ApolloServer } from "apollo-server-micro"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+
+const typeDefs = gql`
+  enum ProjectType {
+    iOS
+    Android
+    IoT
+    Web
+  }
+
+  type ProjectLink {
+    id: ID
+    type: ProjectType!
+    url: String!
+    project: Project
+    project_id: Int
+  }
+
+  type Project {
+    id: ID
+    title: String!
+    description: String!
+    logo: String
+    role: String!
+    year: Int!
+    links: [ProjectLink!]
+  }
+
+  type Query {
+    project_types: [ProjectType]
+    project(id: ID!): Project
+    projects: [Project]
+    project_link(project: ID!): [ProjectLink]
+    project_links: [ProjectLink]
+  }
+`
+
+const prisma = new PrismaClient()
+
+const resolvers = {
+  Query: {
+    project_types: () => ["iOS", "Android", "IoT", "Web"],
+    project: (_parent: unknown, args: { id: unknown }) =>
+      prisma.project.findUnique({ where: { id: Number(args.id) } }),
+    projects: () =>
+      prisma.project.findMany({
+        include: {
+          links: true,
+        },
+      }),
+    project_link: async (_parent: unknown, args: { project: unknown }) => {
+      return prisma.projectLink.findMany({
+        where: { projectId: Number(args.project) },
+      })
+    },
+    project_links: () => prisma.projectLink.findMany(),
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const startServer = server.start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer
+  await server.createHandler({
+    path: "/api",
+  })(req, res)
+}
