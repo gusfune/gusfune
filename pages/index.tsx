d8e2c3b3a7ddfce3e05507690b397e6c311005a4
Gus Fune <gus@offscript.io>
2021-10-28 18:38:09 +0200

    Quick fix on loader of stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,18 @@
 import { GetStaticProps } from "next"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, Recommendation, ProjectLink } from "@prisma/client"
 import Image from "next/image"
 import { NextSeo } from "next-seo"
 import Layout from "components/Layout"
-import {
-  useGetProjectsQuery,
-  GetProjectsQuery,
-  GetRecommendationsQuery,
-  useGetRecommendationsQuery,
-} from "lib/graphql"
-import { client } from "lib/graphql-client"
+import { GetProjectsQuery } from "lib/graphql"
 import { ProjectList } from "components/Project"
 import { RecommendationList } from "components/Recommendation"
 
 export const getStaticProps: GetStaticProps = async () => {
   try {
     const prisma = new PrismaClient()
-    const projects = await prisma.project.findMany({
+    const projects: {
+      links: ProjectLink[]
+    }[] = await prisma.project.findMany({
       orderBy: { year: "desc" },
       select: {
         createdAt: false,
@@ -24,7 +20,8 @@ export const getStaticProps: GetStaticProps = async () => {
         links: true,
       },
     })
-    const recommendations = await prisma.recommendation.findMany()
+    const recommendations: Recommendation[] =
+      await prisma.recommendation.findMany()
     return {
       props: {
         initialData: {
@@ -49,60 +46,41 @@ export const getStaticProps: GetStaticProps = async () => {
 interface Props {
   initialData: {
     projects?: GetProjectsQuery | null
-    recommendations?: GetRecommendationsQuery | null
+    recommendations?: Recommendation[] | null
   }
 }
 
-const HomePage = ({ initialData }: Props) => {
-  const { data: projects } = useGetProjectsQuery<GetProjectsQuery>(
-    client,
-    {},
-    {
-      initialData: initialData.projects ?? undefined,
-    }
-  )
-  const { data: recommendations } =
-    useGetRecommendationsQuery<GetRecommendationsQuery>(
-      client,
-      {},
-      {
-        initialData: initialData.recommendations ?? undefined,
-      }
-    )
-  return (
-    <Layout title="Home" isHome>
-      <NextSeo
-        title="Gus Fune"
-        description="I'm a highly technical Web &amp; Mobile Product Manager who delivered over 100 products to scale."
-        canonical="https://gusfune.com"
-      />
-      <header className="mb-8" role="banner">
-        <div className="flex flex-row items-center space-x-8 align-middle">
-          <Image
-            src="/gusfune.jpg"
-            alt="Gus Fune"
-            width={180}
-            height={180}
-            className="rounded-full"
-          />
-          <div>
-            <h1 className="mb-4 text-4xl font-light">
-              Hi, I&apos;m <span className="font-semibold">Gus Fune</span>
-            </h1>
-            <h2 className="text-3xl font-light">
-              I&apos;m a CTO who delivered over 100 products to scale.
-            </h2>
-          </div>
-        </div>
-      </header>
-      <section role="main">
-        <ProjectList initialData={projects} />
-        <RecommendationList
-          initialData={recommendations as GetRecommendationsQuery}
+const HomePage = ({}: Props) => (
+  <Layout title="Home" isHome>
+    <NextSeo
+      title="Gus Fune"
+      description="I'm a highly technical Web &amp; Mobile Product Manager who delivered over 100 products to scale."
+      canonical="https://gusfune.com"
+    />
+    <header className="mb-8" role="banner">
+      <div className="flex flex-row items-center space-x-8 align-middle">
+        <Image
+          src="/gusfune.jpg"
+          alt="Gus Fune"
+          width={180}
+          height={180}
+          className="rounded-full"
         />
-      </section>
-    </Layout>
-  )
-}
+        <div>
+          <h1 className="mb-4 text-4xl font-light">
+            Hi, I&apos;m <span className="font-semibold">Gus Fune</span>
+          </h1>
+          <h2 className="text-3xl font-light">
+            I&apos;m a CTO who delivered over 100 products to scale.
+          </h2>
+        </div>
+      </div>
+    </header>
+    <section role="main">
+      <ProjectList />
+      <RecommendationList />
+    </section>
+  </Layout>
+)
 
 export default HomePage
