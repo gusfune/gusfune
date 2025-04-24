7946dc7f1fd68adab145ea4b5a20e90194186f6b
Gus Fune <gusfune@users.noreply.github.com>
2025-04-24 10:02:23 -0500

    refactor: :chart_with_upwards_trend: changed cooke from iubenda to ketch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,35 +65,11 @@ export default function RootLayout({
           are free, are part of the collective mind.
         </p>
         <Analytics />
-        <Script
-          strategy="beforeInteractive"
-          id="iubenda-cookie-solution"
-          type="text/javascript"
-        >
+        <Script id="ketch-analytics">
           {`
-var _iub = _iub || [];
-_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"enableFadp":true,"enableLgpd":true,"enableUspr":true,"fadpApplies":true,"floatingPreferencesButtonDisplay":"anchored-center-right","lang":"en-GB","perPurposeConsent":true,"preferenceCookie":{"expireAfter":180},"siteId":3634966,"usprApplies":true,"whitelabel":false,"cookiePolicyId":82859734, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"ownerName":"Gus Fune","position":"bottom","rejectButtonDisplay":true,"showTitle":false,"showTotalNumberOfProviders":true }};
-`}
+!function(){window.semaphore=window.semaphore||[],window.ketch=function(){window.semaphore.push(arguments)};var e=new URLSearchParams(document.location.search),n=document.createElement("script");n.type="text/javascript", n.src="https://global.ketchcdn.com/web/v2/config/gus_fune/website_smart_tag/boot.js", n.defer=n.async=!0,document.getElementsByTagName("head")[0].appendChild(n)}();
+          `}
         </Script>
-        <Script
-          strategy="beforeInteractive"
-          id="iubenda-cookie-autoblock"
-          type="text/javascript"
-          src="https://cs.iubenda.com/autoblocking/3634966.js"
-        />
-        <Script
-          strategy="beforeInteractive"
-          id="iubenda-cookie-stub"
-          type="text/javascript"
-          src="//cdn.iubenda.com/cs/gpp/stub.js"
-        />
-        <Script
-          strategy="beforeInteractive"
-          id="iubenda-cookie-js"
-          type="text/javascript"
-          src="//cdn.iubenda.com/cs/iubenda_cs.js"
-          async
-        />
         <Script id="ph-analytics">
           {`
 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
